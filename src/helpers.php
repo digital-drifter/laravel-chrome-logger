f31eefb48288accea4be959a198052c34b5fe80e
Brian Lee <brian@vandepoelproductions.com>
2018-04-06 12:06:49 -0600

    updates to the helper function

--- src/helpers.php
+++ src/helpers.php
@@ -3,8 +3,21 @@
 use DigitalDrifter\LaravelChromeLogger\Facades\LaravelChromeLogger;
 
 if (!function_exists('console')) {
-    function console(string $level = 'log', array $parameters)
+    /**
+     * @param string $level
+     * @param array ...$parameters
+     * @return mixed
+     */
+    function console()
     {
+        $level      = 'log';
+        $parameters = func_get_args();
+
+        if (count($parameters) > 1) {
+            $level      = $parameters[0];
+            $parameters = array_slice($parameters, 1);
+        }
+
         return LaravelChromeLogger::$level(...$parameters);
     }
 }
